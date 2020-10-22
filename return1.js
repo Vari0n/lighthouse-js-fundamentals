200c8a9af0bc1c5ffaaabbcdd8c43b5116a95319
Kenly Polley <Kenly_P@yahoo.ca>
2020-10-22 20:32:22 +0000

    understanding the difference of return statement and console.log

---
+++ return1.js
@@ -0,0 +1,21 @@
+
+//console.log:
+
+const sayHelloToConsole = function (name) {
+	console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+
+//return: 
+
+
+const returnSayHello = function (name) {
+	return "Hello, " + name;
+
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
+
